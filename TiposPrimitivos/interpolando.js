f1df24b4bb7a626dcd9780ae6d1e8409b8663219
Georgi Santana <georgi.andersson@hotmail.com>
2024-01-25 12:40:29 -0300

    Add arquivo de intepolação

---
+++ TiposPrimitivos/interpolando.js
@@ -0,0 +1,27 @@
+//Variáveis e tipos de dados
+var name;
+
+// Atribuição de valor
+name = "Georgi";
+
+// O tipo de dado que foi colocado na variável
+// console.log(typeof name)
+
+//agrupamento de declaração
+let age, isHuman
+
+age = 36
+isHuman = true
+
+// multiplos argumentos na funcao
+// console.log(name, age, isHuman)
+
+// concatenando valores
+
+console.log("o " + name + " tem " + age + "anos ")
+
+
+
+// interpolando valores com templete literals or template strings
+
+console.log(`O ${name} tem ${age} anos.` )
